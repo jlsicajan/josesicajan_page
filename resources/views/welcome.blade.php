eed7787e34a2390bc1440cee1443b3e80891bcfc
Jose Sicajan <sicajancoy@gmail.com>
2019-02-25 01:33:32 -0600

    Main home page with template adapted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,96 +1,212 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="HTML5 Template"/>
+    <meta name="description" content="Profyl - Personal Vcard Resume HTML Template"/>
+    <meta name="author" content="iqonicthemes.in"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <title>Profyl - Personal Vcard Resume HTML Template</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="template/images/favicon.ico"/>
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600&amp;Raleway:400,500,600,700,800"
+          rel="stylesheet">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="template/css/bootstrap.min.css">
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- Themify Fonts -->
+    <link rel="stylesheet" type="text/css" href="template/css/themify-icons.css">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="template/css/font-awesome.min.css">
 
-            .content {
-                text-align: center;
-            }
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" type="text/css" href="template/css/magnific-popup/magnific-popup.css"/>
 
-            .title {
-                font-size: 84px;
-            }
+    <!-- owl-carousel -->
+    <link rel="stylesheet" type="text/css" href="template/css/owl-carousel/owl.carousel.css"/>
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    <!-- Style CSS -->
+    <link rel="stylesheet" type="text/css" href="template/css/style.css">
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
+    <!-- Responsive CSS -->
+    <link rel="stylesheet" type="text/css" href="template/css/responsive.css">
+
+    <!-- custom style -->
+    <link rel="stylesheet" href="template/css/custom.css"/>
+
+</head>
+
+<body>
+
+<!-- loading -->
+
+<div id="loading" class="purple-bg">
+    <div id="loading-center">
+        <div data-loader="spinner"></div>
+    </div>
+</div>
+
+<!-- loading End -->
+
+
+<!-- Header  -->
+<header class="header">
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="navbar fixed-top navbar-expand-lg navbar-light white-bg">
+                <div class="col-md-3 purple-bg">
+                    <a class="navbar-brand" href="index.html">
+                        <br>
+                    </a>
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                <div class="col-md-9">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="ti-menu"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto w-100 justify-content-end">
+                            <li class="nav-item active">
+                                <a class="nav-link" data-type="page-transition" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-type="page-transition" href="about-us.html">About me</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-type="page-transition" href="resume.html">resume</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-type="page-transition" href="services.html">services</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-type="page-transition" href="portfolio.html">portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-type="page-transition" href="blog.html">blog</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-type="page-transition" href="contact-us.html">contact</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+            </nav>
+        </div>
+    </div>
+</header>
+<!-- Header End -->
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+
+<!-- Begin Content -->
+<div class="main-container">
+    <!-- About us -->
+    <section class="me-home wrapper">
+        <div class="container-fluid h-100">
+            <div class="row row-eq-height h-100">
+                <div class="col-md-3 purple-bg">
+                    <div class="iq-objects">
+                        <!-- BACKGROUND PARTICLES -->
+                        <div id="particles-js"></div>
+
+                        <span class="iq-objects-01">
+                                    <img class="img-fluid" src="template/images/01.png" alt="drive">
+                                </span>
+                    </div>
+                </div>
+                <div class="col-md-9 main-content align-self-center">
+                    <div id="boxscroll" class="iq-home">
+                        <div class="home-deta iq-mt-90">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="boder-img">
+                                        <img src="img/jl_main_profile.jpg" class="img-fluid" alt="#">
+                                    </div>
+                                </div>
+                                <div class="col-md-8 content-deta">
+                                    <h2 class="iq-tw-8 ">Hello,<br>I Am Jose Sicajan</h2>
+                                    <div id="typer"></div>
+                                    <p class="lead iq-tw-4">I'm a passionate web developer, perfectionist, with a high
+                                        sense of responsibility, I like to get into new projects and leave a part of
+                                        myself in them, giving importance to the smallest details. I have worked with
+                                        several international clients and local clients in Guatemala and I have learned
+                                        that constant communication with the client is a very effective tool in
+                                        achieving objectives. Not only do I like web development, I have also learned
+                                        how to create Android Apps and desktop applications.</p>
+                                    <a class="button iq-mt-30" href="about-us.html">Download my CV</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+    </section>
+    <!-- About Us End -->
+</div>
+<!-- Main Content End -->
+
+
+<!-- Footer -->
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 purple-bg">
+            </div>
+            <div class="col-md-9">
+                <ul class="follow-us pull-right">
+                    <li>Follow Me :</li>
+                    <li><a href="https://www.linkedin.com/in/sicajancoy/"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://github.com/jlsicajan"><i class="fa fa-github"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer End -->
+
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script type="text/javascript" src="template/js/jquery-min.js"></script>
+<script type="text/javascript" src="template/js/popper.min.js"></script>
+<script type="text/javascript" src="template/js/bootstrap.min.js"></script>
+
+<!-- jquery-appear -->
+<script type="text/javascript" src="template/js/jquery.appear.js"></script>
+
+<!-- owl-carousel -->
+<script type="text/javascript" src="template/js/owl-carousel/owl.carousel.min.js"></script>
+
+<!-- jquery-typer -->
+<script type="text/javascript" src="template/js/jquery.typer.js"></script>
+
+<!-- jquery-isotope -->
+<script type="text/javascript" src="template/js/isotope.pkgd.min.js"></script>
+
+<!-- Magnific-Popup -->
+<script type="text/javascript" src="template/js/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+<!-- jquery Nicescroll -->
+<script type="text/javascript" src="template/js/jquery.nicescroll.min.js"></script>
+
+<!-- jquery Retina -->
+<script type="text/javascript" src="template/js/retina.min.js"></script>
+
+<!-- Particles -->
+<script type="text/javascript" src="template/js/particles/particles.min.js"></script>
+<script type="text/javascript" src="template/js/particles/app.js"></script>
+
+<!-- Custom -->
+<script type="text/javascript" src="template/js/custom.js"></script>
+
+</body>
+
 </html>
